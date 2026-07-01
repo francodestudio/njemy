6550c0f595a6dfcf0e09cfaa767c8ded8061656b
francodestudio <francodestudio@gmail.com>
2026-07-01 17:21:59 -0600

    chore: set up nativewind configurations

--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -1,17 +1,9 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View,} from "react-native";
 
 export default function Index() {
   return (
-    <View style={styles.container}>
-      <Text>NJEMY</Text>
+    <View className="">
+      <Text>NJEMYd</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
